888765e7ade15585a09658f922862ad85d2d4339
RuslanAkhunov <90615254+FarWG@users.noreply.github.com>
2021-11-09 18:11:59 +0500

    Add files via upload

---
+++ Zad2.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, pr;
+    pr = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        pr = pr * i;
+    }
+    cout << pr;
+    return 0;
+}
